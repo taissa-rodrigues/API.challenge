81376a90539d43d96f18c17aa18fd475807048b5
taissa-rodrigues <taissavrp@gmail.com>
2023-10-07 12:12:46 -0300

    Initial Commit

---
+++ API.challenge/API_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_challengeApp.swift
+//  API.challenge
+//
+//  Created by user on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct API_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API.challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  API.challenge
+//
+//  Created by user on 07/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
